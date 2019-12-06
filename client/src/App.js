c85690ac2ffbef0406a621c141b102a8ea7832e1
rae-glazier <glazier.home.life@gmail.com>
2019-12-06 11:00:33 -0600

    basic code completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Navbar from './Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    playerData: []
+  }
+
+  componentDidMount() {
+    console.log('cDM is running')
+
+    axios.get('http://localhost:5000/api/players')
+      .then(res => {
+        console.log(res);
+
+        this.setState( { playerData: res.data } )
+      })
+    
+  }
+
+  render() {
+
+    return (
+      <div className="App"> 
+
+        <header className="App-header">
+          <Navbar />
+        </header>
+
+        <div>
+          {this.state.playerData.map ( ( player ) => {
+            return(
+              <div>
+                <h2>Name: {player.name} </h2>
+                <h2>Country: {player.country} </h2>
+              </div>
+            )
+          })}
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/Navbar.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+import  useDarkMode  from './useDarkMode';
+
+const Navbar = () => {
+    const [darkMode, setDarkMode] = useDarkMode();
+    const toggleMode = e => {
+      e.preventDefault();
+      setDarkMode(!darkMode);
+    };
+
+    return (
+      <nav className="navbar">
+          <h1>Women's World Cup</h1>
+        <div className="dark-mode__toggle">
+          <button
+            onClick={toggleMode}
+            className={darkMode ? 'toggle toggled' : 'toggle'}
+          >
+          </button>
+    
+        </div>
+      </nav>
+    );
+  };
+
+  export default Navbar;
